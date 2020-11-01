0e0dec8a35a57dfc98f18edf475cec4c4b669f9f
rupanshusoi <rupanshusoi@gmail.com>
2020-11-01 10:13:06 +0530

    Add job-script and src dir

---
+++ job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -l
+#SBATCH --time=00:01:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-core=1
+#SBATCH --ntasks-per-node=12
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=normal
+#SBATCH --constraint=gpu
+
+srun ./src/empty.out
